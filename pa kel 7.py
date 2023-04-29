8a0d49195b4e31340b8420c68736ca012d62b1b0
Normalita06 <127488658+Normalita06@users.noreply.github.com>
2023-04-29 18:35:09 +0800

    Update pa kel 7.py

--- pa kel 7.py
+++ pa kel 7.py
@@ -142,3 +142,103 @@ class LinkedList:
             current_node = current_node.next
             current_index += 1
 
+# nabila
+    def get_id(self, nama):
+        current_node = self.head
+        current_nama = nama
+
+        while current_node is not None:
+            if current_nama == nama:
+                return current_node
+            
+            current_node = current_node.next
+            current_nama = nama
+
+    def get_length(self):
+        current_node = self.head
+        count = 0
+        while current_node:
+            count += 1
+            current_node = current_node.next
+        return count
+
+    def get_node_at_index(self, index):
+        current_node = self.head
+        count = 0
+        while current_node:
+            if count == index:
+                return current_node
+            count += 1
+            current_node = current_node.next
+        return None
+    
+    def beli_makanan(self):
+        if not self.head:
+            print("Tidak Ada Menu Yang Tersedia ! ")
+            return
+
+        print("Pilihlah Menu Yang anda Inginkan !!!")
+        self.tampilan_menu()
+        pilihan = input("Masukkan nomor menu: ")
+        try:
+            pilihan = int(pilihan)
+        except ValueError:
+            print("Masukan tidak valid.")
+            return
+
+        current = self.head
+        i = 1
+        while current:
+            if i == pilihan:
+                if self.saldo < int(current.harga):
+                    print("Saldo Anda tidak mencukupi untuk membeli menu ini !")
+                    return
+                jumlah = input(f"Masukkan jumlah {current.nama} yang akan dibeli: ")
+                try:
+                    jumlah = int(jumlah)
+                except ValueError:
+                    print("Masukan tidak valid.")
+                    return
+                if jumlah <= 0:
+                    print("Jumlah tidak valid.")
+                    return
+                if self.saldo < current.harga * jumlah:
+                    print("Saldo Anda tidak mencukupi untuk membeli menu ini !")
+                    return
+                self.saldo -= current.harga * jumlah
+                print(f"Anda telah membeli {jumlah} {current.nama}. Sisa saldo Anda: {self.saldo}")
+
+                # Create invoice file
+                with open("invoice.txt", "w") as f:
+                    f.write(f"Invoice Pembelian {current.nama}\n")
+                    f.write(f"Jumlah: {jumlah}\n")
+                    f.write(f"Harga Satuan: {current.harga}\n")
+                    f.write(f"Total Harga: {current.harga * jumlah}\n")
+
+
+                return
+            current = current.next
+            i += 1
+
+        print("Nomor menu tidak tersedia !")
+
+    def cek_saldo(self):
+        print(f"Saldo Anda saat ini: {self.saldo}")
+        topup = input("Ingin top up saldo? (y/n): ")
+        if topup.lower() == "y":
+            jumlah = input("Masukkan jumlah saldo yang ingin di-top up: ")
+            try:
+                jumlah = int(jumlah)
+            except ValueError:
+                print("Masukan tidak valid.")
+                return
+            if jumlah <= 0:
+                print("Jumlah tidak valid.")
+                return
+            self.saldo += jumlah
+            print(f"Top up sebesar {jumlah} berhasil. Saldo Anda saat ini: {self.saldo}")
+        elif topup.lower() == "n":
+            return
+        else: 
+            print("Pilihan tidak valid.")
+            return
